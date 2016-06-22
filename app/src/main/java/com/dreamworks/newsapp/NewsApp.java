36a61a44e1f43a49dc7e1365af4fc01af3b93d70
gowri <gowri@alttab.com.au>
2016-06-22 13:52:03 +0530

    NewsApp Declared test variable

--- app/src/main/java/com/dreamworks/newsapp/NewsApp.java
+++ app/src/main/java/com/dreamworks/newsapp/NewsApp.java
@@ -3,7 +3,7 @@ package com.dreamworks.newsapp;
 import android.app.Application;
 
 public class NewsApp extends Application {
-
+    private String test;
     @Override
     public void onCreate() {
         super.onCreate();
