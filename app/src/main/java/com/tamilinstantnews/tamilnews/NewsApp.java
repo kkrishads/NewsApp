4bc93604465dc67a0a4cb585c6419210979c1691
kkrishads <kkrishads@gmail.com>
2016-09-30 14:02:39 +0530

    update commit

--- app/src/main/java/com/tamilinstantnews/tamilnews/NewsApp.java
+++ app/src/main/java/com/tamilinstantnews/tamilnews/NewsApp.java
@@ -2,10 +2,15 @@ package com.tamilinstantnews.tamilnews;
 
 import android.app.Application;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class NewsApp extends Application {
     private String test;
     @Override
     public void onCreate() {
         super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 }
--- app/src/main/java/com/tamilinstantnews/tamilnews/NewsDetails_Activity.java
+++ app/src/main/java/com/tamilinstantnews/tamilnews/NewsDetails_Activity.java
@@ -15,12 +15,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.ads.Ad;
+import com.facebook.ads.AdError;
+import com.facebook.ads.AdListener;
+import com.facebook.ads.AdSettings;
+import com.facebook.ads.AdSize;
+import com.facebook.ads.AdView;
+import com.facebook.ads.InterstitialAd;
+import com.facebook.ads.InterstitialAdListener;
 import com.squareup.picasso.Picasso;
 import com.tamilinstantnews.tamilnews.R;
 import com.tamilinstantnews.tamilnews.utils.DeviceUtils;
+import com.vungle.publisher.EventListener;
+import com.vungle.publisher.VunglePub;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -29,7 +40,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-public class NewsDetails_Activity extends AppCompatActivity {
+public class NewsDetails_Activity extends AppCompatActivity implements InterstitialAdListener {
 
     private String newsDetails, newsImgUrl;
     private AsyncDetails asyncDetails;
@@ -38,10 +49,128 @@ public class NewsDetails_Activity extends AppCompatActivity {
     private String headings, dateTime, publisherName, url;
     private ProgressDialog dialog;
     private FloatingActionButton fabShare;
+    private InterstitialAd interstitialAd;
+    private AdView adView;
+    private boolean adLoaded=false;
+    final VunglePub vunglePub = VunglePub.getInstance();
+    private void loadInterstitialAd(){
+        adLoaded=false;
+        interstitialAd = new InterstitialAd(this, "1559302264365604_1559305931031904");
+        interstitialAd.setAdListener(this);
+        interstitialAd.loadAd();
+    }
+    @Override
+    protected void onDestroy() {
+        adView.destroy();
+        super.onDestroy();
+    }
+
+    private void loadBannerAd(){
+        RelativeLayout adViewContainer = (RelativeLayout) findViewById(R.id.adViewContainer);
+
+        final String app_id = "57ee0fea062879e91d00002c";
+
+        // initialize the Publisher SDK
+        vunglePub.init(this, app_id);
+
+        vunglePub.setEventListeners(vungleDefaultListener, vungleSecondListener);
+
+        adView = new AdView(this, "1559302264365604_1559305824365248", AdSize.BANNER_320_50);
+//        AdSettings.addTestDevice("79890a473a7548850555943a5921cb5c");
+        adViewContainer.addView(adView);
+        adView.loadAd();
+
+        adView.setAdListener(new AdListener() {
+
+            @Override
+            public void onError(Ad ad, AdError error) {
+                // Ad failed to load.
+                // Add code to hide the ad's view
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                // Ad was loaded
+                // Add code to show the ad's view
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Use this function to detect when an ad was clicked.
+            }
+
+        });
+    }
+
+    private final EventListener vungleDefaultListener = new EventListener() {
+        @Deprecated
+        @Override
+        public void onVideoView(boolean isCompletedView, int watchedMillis, int videoDurationMillis) {
+            // This method is deprecated and will be removed. Please use onAdEnd() instead.
+        }
+
+        @Override
+        public void onAdStart() {
+            // Called before playing an ad.
+        }
+
+        @Override
+        public void onAdUnavailable(String reason) {
+            // Called when VunglePub.playAd() was called but no ad is available to show to the user.
+        }
+
+        @Override
+        public void onAdEnd(boolean wasSuccessfulView, boolean wasCallToActionClicked) {
+            // Called when the user leaves the ad and control is returned to your application.
+        }
+
+        @Override
+        public void onAdPlayableChanged(boolean isAdPlayable) {
+            // Called when ad playability changes.
+            Log.d("DefaultListener", "This is a default eventlistener.");
+            final boolean enabled = isAdPlayable;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // Called when ad playability changes.
+//                    setButtonState(buttonPlayAd, enabled);
+//                    setButtonState(buttonPlayAdIncentivized, enabled);
+//                    setButtonState(buttonPlayAdOptions, enabled);
+                }
+            });
+        }
+    };
+
+    private final EventListener vungleSecondListener = new EventListener() {
+        // Vungle SDK allows for multiple listeners to be attached. This secondary event listener is only
+        // going to print some logs for now, but it could be used to Pause music, update a badge icon, etc.
+        @Deprecated
+        @Override
+        public void onVideoView(boolean isCompletedView, int watchedMillis, int videoDurationMillis) {}
+
+        @Override
+        public void onAdStart() {}
+
+        @Override
+        public void onAdUnavailable(String reason) {}
+
+        @Override
+        public void onAdEnd(boolean wasSuccessfulView, boolean wasCallToActionClicked) {}
+
+        @Override
+        public void onAdPlayableChanged(boolean isAdPlayable) {
+            Log.d("SecondListener", String.format("This is a second event listener! Ad playability has changed, and is now: %s", isAdPlayable));
+        }
+    };
+    private void PlayAd() {
+        vunglePub.playAd();
+    }
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_activity);
+        loadBannerAd();
+        loadInterstitialAd();
         if (getIntent().getStringExtra("HEADINGS") != null) {
             headings = getIntent().getStringExtra("HEADINGS");
             dateTime = getIntent().getStringExtra("DATE_TIME");
@@ -77,6 +206,8 @@ public class NewsDetails_Activity extends AppCompatActivity {
                 sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, ""+txtHeading.getText().toString());
                 sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
                 startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                if(adLoaded)
+                interstitialAd.show();
             }
         });
 
@@ -95,6 +226,31 @@ public class NewsDetails_Activity extends AppCompatActivity {
             Toast.makeText(NewsDetails_Activity.this, "No Internet Conenction", Toast.LENGTH_SHORT).show();
             finish();
         }
+    }
+
+    @Override
+    public void onInterstitialDisplayed(Ad ad) {
+
+    }
+
+    @Override
+    public void onInterstitialDismissed(Ad ad) {
+
+    }
+
+    @Override
+    public void onError(Ad ad, AdError adError) {
+
+    }
+
+    @Override
+    public void onAdLoaded(Ad ad) {
+        adLoaded=true;
+    }
+
+    @Override
+    public void onAdClicked(Ad ad) {
+
     }
 
     class AsyncDetails extends AsyncTask<Void, Void, Void> {
@@ -146,6 +302,8 @@ public class NewsDetails_Activity extends AppCompatActivity {
                 txtHeading.setText("" + Html.fromHtml(headings));
                 if (newsImgUrl != null && !newsImgUrl.isEmpty()) {
                     Picasso.with(NewsDetails_Activity.this).load(newsImgUrl).placeholder(R.mipmap.ic_launcher).into(imgNews);
+                } else {
+                    imgNews.setVisibility(View.GONE);
                 }
             }
 
